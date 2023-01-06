3807a36240118c05bcde1dc6134dc8dce4037dfd
saad khan <saadkhan108b@gmail.com>
2023-01-06 20:18:36 +0530

    Added emailRegex

--- phoneAndEmail.py
+++ phoneAndEmail.py
@@ -1,25 +1,45 @@
 #! python3
+# Author    - Saad khan
+# Date      - 1/6/2023
 # phoneAndEmail.py - Extract email and phone number from and on clipboard
 
 import pyperclip, re
 
-numbers = '''
+numbersAndEmails = '''
     +919820397879
     +91-9820397879
+    azizzaid308@gmail.com
     +91 9820397879
     009920577934
+    saadkhan108b@gmail.com
     00 9920577934
     00-9920577934
+    mshahid108b@gmail.com
     8850701287
     2223336665
     
 
     '''
 
+# Creating phone regex
 
-phoneRegex = re.compile(r" (\+91|0{0,2})?([-|\s])?([6789]\d{9})")
+phoneRegex = re.compile(r''' 
+    (\+91|0{0,2})?  #Country code
+    ([-|\s])?       #Separator
+    ([6789]\d{9})   #Phone Number
+    ''', re.VERBOSE)
 
-results = phoneRegex.findall(numbers)
-#results = phoneRegex.finditer(numbers)
-for res in results:
-    print(f"{res[0]}{res[1]}{res[2]}")
+# Creating email regex
+
+emailRegex = re.compile(r'''(
+    ([A-Za-z0-9.+-_%]+)     #Username 
+    @                       #@ symbol
+    [A-Za-z0-9.+-_%]+       #Domain Name
+    \.[a-zA-Z]{2,4}         #Dot extension
+    )''', re.VERBOSE)
+
+
+
+results = phoneRegex.findall(numbersAndEmails)
+
+print(results)
